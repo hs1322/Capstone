7c5249753fecba4b73bfbbe95c14d8605192d807
hs1322 <90660378+hs1322@users.noreply.github.com>
2023-05-10 18:04:04 +0900

    Update hcg.py

--- hcg.py
+++ hcg.py
@@ -11,12 +11,14 @@ with mp_face_detection.FaceDetection(
     model_selection=0, min_detection_confidence=0.5) as face_detection:
   # model_selection -> 모델 인덱스 0 = 카메라 2m 이내 부분적 모델 촬영 / 1 = 5m 이내에서 전신 모델
   # min_detection_confidenc -> 검출 성공 얼굴의 검출 모델의 신뢰값 ([0.0, 1.0]) 기본 값 0.5
+	
   while cap.isOpened():
     success, image = cap.read()
     if not success:
       print("카메라를 찾을수 없습니다.")
       # 비디오 파일 continue를 사용, 웹캠 break 사용
       break
+
 	# 이미지를 좌우를 반전, BGR 이미지를 RGB로 변환
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
 	# 성능 향상 - 이미지 작성 여부 False 설정
