caf737194adf588befec86e57ad78687bd52b68c
hs1322 <90660378+hs1322@users.noreply.github.com>
2023-11-07 12:28:00 +0900

    Update hcg.py

--- hcg.py
+++ hcg.py
@@ -1,37 +1,47 @@
 print("1801301 황찬기")
 
-# MediaPipe Face Detection을 이용한 얼굴 인식 (좌/우 눈, 코 끝, 입 중심, 좌/우 귀 이주)
+# dlib을 이용한 얼굴 인식과 불 필요한 영역 흰색 채우기
+import sys
+import dlib
 import cv2
-import mediapipe as mp
-mp_face_detection = mp.solutions.face_detection
-mp_drawing = mp.solutions.drawing_utils
+import numpy as np
 
-cap = cv2.VideoCapture(0)
-with mp_face_detection.FaceDetection(
-    model_selection=0, min_detection_confidence=0.5) as face_detection:
-  # model_selection -> 모델 인덱스 0 = 카메라 2m 이내 부분적 모델 촬영 / 1 = 5m 이내에서 전신 모델
-  # min_detection_confidenc -> 검출 성공 얼굴의 검출 모델의 신뢰값 ([0.0, 1.0]) 기본 값 0.5
-	
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("카메라를 찾을수 없습니다.")
-      # 비디오 파일 continue를 사용, 웹캠 break 사용
-      break
+detector = dlib.get_frontal_face_detector()
+cam = cv2.VideoCapture(0)
+color_green = (0, 255, 0)
+line_width = 3
 
-	# 이미지를 좌우를 반전, BGR 이미지를 RGB로 변환
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-	# 성능 향상 - 이미지 작성 여부 False 설정
-    image.flags.writeable = False
-    results = face_detection.process(image)
+while True:
+    ret_val, img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # 얼굴 검출
+    dets = detector(gray)
 
-    # 영상에 얼굴 감지 주석 그리기 기본값 : True.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.detections:
-      for detection in results.detections:
-        mp_drawing.draw_detection(image, detection)
-    cv2.imshow('MediaPipe Face Detection', image)
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
-cap.release()
+    # 이미지 크기
+    height, width = gray.shape
+    
+    # 흑백 이미지로 마스크 생성 (검은 배경)
+    mask = np.zeros((height, width), dtype=np.uint8)
+
+    # 얼굴이 검출되면 해당 얼굴 영역을 제외한 좌우 영역을 흰색으로 칠함
+    for det in dets:
+        left, top, right, bottom = det.left(), det.top(), det.right(), det.bottom()
+        
+        # 얼굴 영역을 제외한 좌우 영역을 흰색으로 칠함
+        mask[:, left:right] = 255  # 얼굴 영역을 제외한 좌우 영역을 흰색으로
+
+        # 얼굴 영역 표시
+        cv2.rectangle(img, (left, top), (right, bottom), color_green, line_width)
+
+    # 마스크를 이용하여 원본 이미지의 좌우 영역을 흰색으로 채움
+    img[mask == 0] = 255
+
+    # 얼굴 영역을 흰색으로 채운 이미지를 보여줌
+    cv2.imshow('Face and Surrounding Area', img)
+
+    if cv2.waitKey(1) == 27:
+        break  # esc to quit
+
+# 창 닫기
+cv2.destroyAllWindows()
